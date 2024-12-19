be00dacbf386d376195fb32c67232ccf131929b2
Alfazal007 <alfazal7777@gmail.com>
2024-12-20 02:29:15 +0530

    added shadcn and tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,9 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { SignUp } from "./components/Signup";
-import { SignIn } from "./components/Signin";
-
-export interface User {
-	accessToken: string;
-	refreshToken: string;
-	username: string;
-	id: string;
-}
-
 function App() {
-
-	const router = createBrowserRouter([
-		{
-			path: "/signup",
-			element: <SignUp />,
-		},
-		{
-			path: "/signin",
-			element: <SignIn />,
-		},
-	]);
-
 	return (
-		<>
-			<UserProvider>
-				<RouterProvider router={router} />
-			</UserProvider>
-		</>
-	);
+		<div className="text-3xl font-bold underline">
+			Hello world!
+		</div>
+	)
 }
 
-export default App;
+export default App 
